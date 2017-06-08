4322b3b3ea09f5d395d1e735f4ed8c7e3467eb17
derRaab <github.com@derRaab.com>
2017-06-08 21:52:16 +0200

    Trace start and exit on flash target

--- test/hx/TestMainHx.hx
+++ test/hx/TestMainHx.hx
@@ -4,10 +4,16 @@ class TestMainHx {
 
     static function main() {
 
+        trace( "TestMainHx.main()" );
+
         var r = new haxe.unit.TestRunner();
 
         r = de.superclass.test.haxe.TestCases.addAll( r );
 
         r.run();
+
+        #if flash
+        flash.system.System.exit(0);
+        #end
     }
 }
