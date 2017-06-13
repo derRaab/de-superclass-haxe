f63bc361dcd07563d2b7fab536b62550fdd94415
derRaab <github.com@derRaab.com>
2017-06-13 10:46:45 +0200

    Added DateUtil.getDefaultTimezoneOffsetSeconds()

--- src/hx/de/superclass/haxe/util/DateUtil.hx
+++ src/hx/de/superclass/haxe/util/DateUtil.hx
@@ -27,6 +27,8 @@ package de.superclass.haxe.util;
 **/
 class DateUtil {
 
+    private static var _localTimezoneOffsetSeconds : Float = -1;
+
     private inline static var _TIME_SEPARATOR : String = "T";
     private inline static var _TIME_ZONE_DESIGNATOR_UTC : String = "Z";
     private inline static var _TIME_ZONE_DESIGNATOR_PLUS : String = "+";
@@ -231,5 +233,44 @@ class DateUtil {
         }
 
         return null;
+    }
+
+    /**
+		Returns the default timezone offset in seconds for created `Date` instances. (GMT + 1 would return 3600).
+
+		Since the Haxe `Date` implementation doesn't provide an UTC api for all platforms, this is a convienient way to detect
+		how much timezone offset (in seconds) a new created `Date` instance includes.
+
+		If this returns 0, it just means no timezone offset is available. It doesn't ensure this date instance represents
+		actual UTC time, but that the platform doesn't apply any offset to new instances!
+	**/
+    public static function getDefaultTimezoneOffsetSeconds() : Float {
+
+        var localTimezoneOffsetSeconds : Float = _localTimezoneOffsetSeconds;
+        if ( localTimezoneOffsetSeconds == -1 ) {
+
+            _localTimezoneOffsetSeconds = localTimezoneOffsetSeconds = _createDefaultTimezoneOffsetSeconds();
+        }
+        return localTimezoneOffsetSeconds;
+    }
+
+    private static function _createDefaultTimezoneOffsetSeconds() : Float {
+
+        var offsetSeconds : Float = 0;
+
+        var unixTimestampStartDate : Date = Date.fromTime( TimeUtil.daysToMilliseconds( 0 ) );
+        var unixTimestampStartDateHours : Int = unixTimestampStartDate.getHours();
+        if ( unixTimestampStartDateHours != 0 ) {
+
+            var isNegativeTimezone : Bool = ( unixTimestampStartDate.getFullYear() < 1970 );
+            if ( isNegativeTimezone ) {
+
+                unixTimestampStartDateHours = unixTimestampStartDateHours - 24;
+            }
+
+            offsetSeconds +=  TimeUtil.millisecondsToSeconds( TimeUtil.hoursToMilliseconds( unixTimestampStartDateHours ) );
+        }
+
+        return offsetSeconds;
     }
 }
