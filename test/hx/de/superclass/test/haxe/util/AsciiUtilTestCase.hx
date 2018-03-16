97e3d5f3726d37e9cc2b1df22b2155e197683794
derRaab <github.com@derRaab.com>
2018-03-16 17:21:07 +0100

    Add more AsciiUtil tests

--- test/hx/de/superclass/test/haxe/util/AsciiUtilTestCase.hx
+++ test/hx/de/superclass/test/haxe/util/AsciiUtilTestCase.hx
@@ -8,12 +8,15 @@ class AsciiUtilTestCase extends TestCase {
     public function testHasAccents() {
 
         assertTrue( AsciiUtil.hasAccents( "ä" ) );
+        assertTrue( AsciiUtil.hasAccents( "aä" ) );
         assertFalse( AsciiUtil.hasAccents( "a" ) );
     }
 
     public function testRemoveAccents() {
 
         assertEquals( AsciiUtil.removeAccents( "ä" ), "a" );
+        assertEquals( AsciiUtil.removeAccents( "aä" ), "aa" );
         assertEquals( AsciiUtil.removeAccents( "ö" ), "o" );
+        assertEquals( AsciiUtil.removeAccents( "oö" ), "oo" );
     }
 }
